403b7beed5083f6f1bd1e181b02e196ba2024324
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-01-10 01:51:00 +0000

    added Persistence in Firebase config file

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,6 +23,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+//export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP,
+                              {
+                                persistence: getReactNativePersistence(AsyncStorage)
+                              });
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 //const analytics = getAnalytics(app);
