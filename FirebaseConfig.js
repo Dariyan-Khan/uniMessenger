80b93c0a1e61becd5f8520dd2c775f30c43388c7
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-01-13 23:17:32 +0000

    changes

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth"; //Might not need this anymore
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -23,11 +23,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-//export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
 
-export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP,
-                              {
-                                persistence: getReactNativePersistence(AsyncStorage)
-                              });
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+
+// export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP,
+//                               {
+//                                 persistence: getReactNativePersistence(AsyncStorage)
+//                               });
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 //const analytics = getAnalytics(app);
